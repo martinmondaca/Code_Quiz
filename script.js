9dea52c5c875c87d65a3b8ddff9f3b21446194f6
Martin M <martin7mondaca@gmail.com>
2021-02-01 04:44:24 -0700

    added missing semicolons to code

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ var currentScore = document.querySelector("#currentScore");
 var beginButton = document.querySelector("#beginQuiz");
 var beginSection = document.querySelector("#beginSection");
 var quizSection = document.querySelector("#quizHolder");
-var currentQuestion = document.querySelector("#currentQuestion")
-var optionOne = document.querySelector("#answerA")
-var optionTwo = document.querySelector("#answerB")
-var optionThree = document.querySelector("#answerC")
-var optionFour = document.querySelector("#answerD")
-var finalResultScreen = document.querySelector("#finalResults")
-var finalScore = document.querySelector("#finalScore")
-var leaderboardView = document.querySelector("#leaderboardBox")
-var saveScoreBtn = document.querySelector("#saveYourScore")
-var nameInput = document.querySelector("#nameInput")
-var seeScoresBtn = document.querySelector("#seeScores")
-var leaderboardScores = document.querySelector("#leaderboard")
-var clearScoresBtn = document.querySelector("#clearScores")
-var retakeQuizBtn = document.querySelector("#retakeQuiz")
+var currentQuestion = document.querySelector("#currentQuestion");
+var optionOne = document.querySelector("#answerA");
+var optionTwo = document.querySelector("#answerB");
+var optionThree = document.querySelector("#answerC");
+var optionFour = document.querySelector("#answerD");
+var finalResultScreen = document.querySelector("#finalResults");
+var finalScore = document.querySelector("#finalScore");
+var leaderboardView = document.querySelector("#leaderboardBox");
+var saveScoreBtn = document.querySelector("#saveYourScore");
+var nameInput = document.querySelector("#nameInput");
+var seeScoresBtn = document.querySelector("#seeScores");
+var leaderboardScores = document.querySelector("#leaderboard");
+var clearScoresBtn = document.querySelector("#clearScores");
+var retakeQuizBtn = document.querySelector("#retakeQuiz");
 
 
-var answerButtons = [optionOne, optionTwo, optionThree, optionFour]
+var answerButtons = [optionOne, optionTwo, optionThree, optionFour];
 var score = 0;
 var questionNumber = 0;
 var timer = 60;
@@ -56,18 +56,18 @@ beginButton.addEventListener("click", function () {
         };
     }, 1000);
     displayQuestions(questionNumber);
-    initLocalStorage()
-    renderLearboard()
-    displayLeaderboard()
+    initLocalStorage();
+    renderLearboard();
+    displayLeaderboard();
 });
 
 //countdown timer
 function initiateTimer(timer) {
     timer--;
     timeLeft.innerHTML = timer;
-}
+};
 
-//load up each question
+//load up each question and answers
 function displayQuestions(currentQuestionIndex) {
     enableButtons();
     if (currentQuestionIndex >= 10) {
@@ -83,41 +83,38 @@ function displayQuestions(currentQuestionIndex) {
         optionThree.innerHTML = JSON.stringify(quizQuestions[currentQuestionIndex].answers[2].c).replace(/"/g, "");
         optionFour.innerHTML = JSON.stringify(quizQuestions[currentQuestionIndex].answers[3].d).replace(/"/g, "");
         for (i = 0; i < answerButtons.length; i++) {
-            answerButtons[i].setAttribute("class", "btn btn-light btn-lg btn-block text-left")
+            answerButtons[i].setAttribute("class", "btn btn-light btn-lg btn-block text-left");
         };
     };
 };
 
 optionOne.addEventListener("click", function () {
-    isCorrect = quizQuestions[questionNumber].answers[0].correct
+    isCorrect = quizQuestions[questionNumber].answers[0].correct;
     wrongOrRight(optionOne);
     disableButtons();
     nextQuestion();
-
-})
+});
 
 optionTwo.addEventListener("click", function () {
-    isCorrect = quizQuestions[questionNumber].answers[1].correct
+    isCorrect = quizQuestions[questionNumber].answers[1].correct;
     wrongOrRight(optionTwo);
     disableButtons();
     nextQuestion();
-
-})
+});
 
 optionThree.addEventListener("click", function () {
-    isCorrect = quizQuestions[questionNumber].answers[2].correct
+    isCorrect = quizQuestions[questionNumber].answers[2].correct;
     wrongOrRight(optionThree);
     disableButtons();
     nextQuestion();
-
-})
+});
 
 optionFour.addEventListener("click", function () {
-    isCorrect = quizQuestions[questionNumber].answers[3].correct
+    isCorrect = quizQuestions[questionNumber].answers[3].correct;
     wrongOrRight(optionFour);
     disableButtons();
     nextQuestion();
-})
+});
 
 function wrongOrRight(whatsTheQuestion) {
     if (isCorrect) {
@@ -130,8 +127,8 @@ function wrongOrRight(whatsTheQuestion) {
         score -= 25;
         timer -= 4;
         currentScore.innerHTML = score;
-    }
-}
+    };
+};
 
 //gets next question
 function nextQuestion() {
@@ -139,80 +136,79 @@ function nextQuestion() {
     setTimeout(function () {
         displayQuestions(questionNumber);
     }, 1000);
-}
+};
 
 //disable buttons after user selects answer
 function disableButtons() {
     for (i = 0; i < answerButtons.length; i++) {
-        answerButtons[i].disabled = true
-    }
-}
+        answerButtons[i].disabled = true;
+    };
+};
 
 //enable buttons with new question
 function enableButtons() {
     for (i = 0; i < answerButtons.length; i++) {
         answerButtons[i].disabled = false;
-    }
-}
+    };
+};
 //save scores
 saveScoreBtn.addEventListener("click", function () {
-    initLocalStorage();
-    renderLearboard()
-    var playerName = nameInput.value
+    var playerName = nameInput.value;
     if (playerName === null || playerName === "") {
-        alert("Please provide a name for your score.")
-    }
-    var newScore = playerName + ": " + score
-    var currentLeaderboard = JSON.parse(localStorage.getItem("codeQuizLeaderScores"))
-    currentLeaderboard.push(newScore)
-    localStorage.setItem("codeQuizLeaderScores", JSON.stringify(currentLeaderboard))
-    displayLeaderboard()
-    finalResultScreen.setAttribute("class", "container hide");
-})
+        alert("Please provide a name for your score.");
+    } else {
+        initLocalStorage();
+        renderLearboard();
+        var newScore = playerName + ": " + score;
+        var currentLeaderboard = JSON.parse(localStorage.getItem("codeQuizLeaderScores"));
+        currentLeaderboard.push(newScore);
+        localStorage.setItem("codeQuizLeaderScores", JSON.stringify(currentLeaderboard));
+        displayLeaderboard();
+        finalResultScreen.setAttribute("class", "container hide");
+    };
+});
 
 //see scores
-
 seeScoresBtn.addEventListener("click", function (event) {
-    initLocalStorage()
+    initLocalStorage();
     renderLearboard();
-    displayLeaderboard()
+    displayLeaderboard();
     event.preventDefault();
     leaderboardView.setAttribute("class", "container");
     finalResultScreen.setAttribute("class", "container hide");
     quizSection.setAttribute("class", "container hide");
     beginSection.setAttribute("class", "container hide");
-
-})
+});
 
 //loops through scores saved in local storage and prepends them to Recent Scores
 function displayLeaderboard() {
-    var currentLeaderboard = JSON.parse(localStorage.getItem("codeQuizLeaderScores"))
+    var currentLeaderboard = JSON.parse(localStorage.getItem("codeQuizLeaderScores"));
     for (i = 0; i < currentLeaderboard.length; i++) {
         singleScore = currentLeaderboard[i];
         var para = document.createElement("p");
-        para.classList.add("individualScore")
+        para.classList.add("individualScore");
         para.textContent = singleScore + " points";
-        leaderboardScores.prepend(para)
-    }
-}
+        leaderboardScores.prepend(para);
+    };
+};
+
 //clear leaderboard
 clearScoresBtn.addEventListener("click", function () {
-    localStorage.removeItem("codeQuizLeaderScores")
-    renderLearboard()
-})
+    localStorage.removeItem("codeQuizLeaderScores");
+    renderLearboard();
+});
 
 //empty leaderboard scores on page
 function renderLearboard() {
-    leaderboardScores.innerHTML = ""
-
-}
+    leaderboardScores.innerHTML = "";
+};
 
 //create leaderboard key in local storage
 function initLocalStorage() {
     if (localStorage.getItem("codeQuizLeaderScores") === null) {
-        localStorage.setItem("codeQuizLeaderScores", "[]")
+        localStorage.setItem("codeQuizLeaderScores", "[]");
     };
-}
+};
 
 //quiz questions
 var quizQuestions = [
@@ -297,4 +293,4 @@ var quizQuestions = [
             { d: "d. &lt;dl&gt;", correct: false }
         ]
     }
-]
+];
